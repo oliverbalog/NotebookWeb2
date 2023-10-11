bddd466c10d3294ada9992042fe33bda9d6add39
oliverbalog2 <asperant8@gmail.com>
2023-10-11 19:54:38 +0200

    routes collection

--- public/index.php
+++ public/index.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Helpers\Router;
+require_once __DIR__ . '/../config/config.php';
+require_once __DIR__ . '/../routes/web.php';
 session_start();
 
-?>
+Router::invoke($routes);
---
+++ routes/web.php
@@ -0,0 +1,7 @@
+<?php
+
+use App\Helpers\RouteCollection;
+
+$routes = new RouteCollection();
+
+?>
