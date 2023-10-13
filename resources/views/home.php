3c3e4ef63be4a4d0fbbda78f8ac00aac0e215a72
oliverbalog2 <oliverbalog98@gmail.com>
2023-10-13 16:06:22 +0200

    images

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,38 +1,62 @@
 <?php
 include(APP_ROOT . "/resources/views/layouts/header.php");
 ?>
-<section>
+<section class="mt-4">
     <h4>Hogyan válasszunk notebookot?</h4>
-    <p>
-        A notebook vásárlás első lépése, hogy meghatározzuk a notebook használatának a célját. Munkához, tanuláshoz,
-        játékhoz, esetleg komolyabb teljesítményt igénylő feladatokhoz- mint mondjuk videóvágás- keresünk megfelelő
-        gépet?
-    </p>
+
+    <div class="row mb-2">
+        <div class="col">
+            <p>
+                A notebook vásárlás első lépése, hogy meghatározzuk a notebook használatának a célját. Munkához,
+                tanuláshoz,
+                játékhoz, esetleg komolyabb teljesítményt igénylő feladatokhoz- mint mondjuk videóvágás- keresünk
+                megfelelő
+                gépet?
+            </p>
+        </div>
+        <div class="col-4">
+            <img class="note-img mt-2" src="./images/notebook.jpg" />
+        </div>
+    </div>
+
     <p>
         Amennyiben rendszeresen magunkkal szeretnénk vinni az iskolába vagy a munkahelyünkre, válasszunk a kisebb és
-        könnyebb 11”-os modellek közül, és ne feledkezzünk meg a notebook táskáról sem, ami lehet egy notebook hátizsák
-        vagy tok is, hogy biztonságosan szállíthassuk. Tanuláshoz érdemes elgondolkozni egy még könnyebb és praktikusabb
+        könnyebb 11”-os modellek közül, és ne feledkezzünk meg a notebook táskáról sem, ami lehet egy notebook
+        hátizsák
+        vagy tok is, hogy biztonságosan szállíthassuk. Tanuláshoz érdemes elgondolkozni egy még könnyebb és
+        praktikusabb
         tablet vásárlásán is, vagy válasszunk egy notebook-tablet hibridet.
     </p>
     <p>
-        Fontos megnézni, hogy a notebook operációs rendszerrel rendelkezik e, mert nem mindegyik modell mellé jár, és a
+        Fontos megnézni, hogy a notebook operációs rendszerrel rendelkezik e, mert nem mindegyik modell mellé jár,
+        és a
         szoftver külön megvásárlása drága lehet. Legelterjedtebb operációs rendszer laptopokra a windows 10.
     </p>
+
+    <img id="note-big-img" src="./images/notebook.png" />
 
     <h4>
         Milyen számokra figyeljünk oda?
     </h4>
 
     <p>
-        A notebookok teljesítményét elsősorban a processzor (CPU) teljesítménye határozza meg. Jelenleg két gyártó, az Intel és az AMD kínálatából választhatunk. Az Intelnél növekvő sorrendben Atom, Celeron, Pentium, Core i3, Core i5 és Core i7 processzorokat találhatunk, az AMD-nél a sorrend így alakul: E1, E2, A4, A6, A10, A12.
+        A notebookok teljesítményét elsősorban a processzor (CPU) teljesítménye határozza meg. Jelenleg két gyártó,
+        az Intel és az AMD kínálatából választhatunk. Az Intelnél növekvő sorrendben Atom, Celeron, Pentium, Core
+        i3, Core i5 és Core i7 processzorokat találhatunk, az AMD-nél a sorrend így alakul: E1, E2, A4, A6, A10,
+        A12.
     </p>
 
     <p>
-        Fontos, hogy felmérjük mire szeretnénk pontosan használni a notebookunkat, mert amennyiben nem használjuk ki az erősebb processzorok teljesítményét, sok pénzt elveszíthetünk a választásával, ráadásul többet is fogyaszt. Gamer laptopok esetén a processzor nagysága nem elhanyagolható és egy gamer laptophűtő néhány esetben elengedhetetlen.
+        Fontos, hogy felmérjük mire szeretnénk pontosan használni a notebookunkat, mert amennyiben nem használjuk ki
+        az erősebb processzorok teljesítményét, sok pénzt elveszíthetünk a választásával, ráadásul többet is
+        fogyaszt. Gamer laptopok esetén a processzor nagysága nem elhanyagolható és egy gamer laptophűtő néhány
+        esetben elengedhetetlen.
     </p>
 
     <p>
-        Ne felejtsük el a memória (RAM) méretét is megvizsgálni a választásnál. A notebookok bekapcsoláskor a RAM-ból töltik be a legfontosabb dolgokat (asztal, futó programok). Egy 4GM alatti memóriájú gép esetében éppen ezért úgy érezhetjük, hogy lassú a használata. 
+        Ne felejtsük el a memória (RAM) méretét is megvizsgálni a választásnál. A notebookok bekapcsoláskor a
+        RAM-ból töltik be a legfontosabb dolgokat (asztal, futó programok). Egy 4GM alatti memóriájú gép esetében
+        éppen ezért úgy érezhetjük, hogy lassú a használata.
     </p>
 </section>
 <?php
--- resources/views/layouts/footer.php
+++ resources/views/layouts/footer.php
@@ -7,4 +7,9 @@
 			Footer
 		</div>
 	</div>
+	<div class="row">
+		<div class="col-xs-12 center-block text-center app-title">
+			@2023
+		</div>
+	</div>
 </footer>   
