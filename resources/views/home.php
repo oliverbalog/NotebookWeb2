8ed81b6a6b8d95e0048c5c0a36218a73bc9167e3
oliverbalog2 <oliverbalog98@gmail.com>
2023-10-13 15:41:44 +0200

    contents

--- resources/views/home.php
+++ resources/views/home.php
@@ -1,9 +1,40 @@
 <?php
 include(APP_ROOT . "/resources/views/layouts/header.php");
 ?>
-    <section>
-        This is a notebook selling application!
-    </section>
+<section>
+    <h4>Hogyan válasszunk notebookot?</h4>
+    <p>
+        A notebook vásárlás első lépése, hogy meghatározzuk a notebook használatának a célját. Munkához, tanuláshoz,
+        játékhoz, esetleg komolyabb teljesítményt igénylő feladatokhoz- mint mondjuk videóvágás- keresünk megfelelő
+        gépet?
+    </p>
+    <p>
+        Amennyiben rendszeresen magunkkal szeretnénk vinni az iskolába vagy a munkahelyünkre, válasszunk a kisebb és
+        könnyebb 11”-os modellek közül, és ne feledkezzünk meg a notebook táskáról sem, ami lehet egy notebook hátizsák
+        vagy tok is, hogy biztonságosan szállíthassuk. Tanuláshoz érdemes elgondolkozni egy még könnyebb és praktikusabb
+        tablet vásárlásán is, vagy válasszunk egy notebook-tablet hibridet.
+    </p>
+    <p>
+        Fontos megnézni, hogy a notebook operációs rendszerrel rendelkezik e, mert nem mindegyik modell mellé jár, és a
+        szoftver külön megvásárlása drága lehet. Legelterjedtebb operációs rendszer laptopokra a windows 10.
+    </p>
+
+    <h4>
+        Milyen számokra figyeljünk oda?
+    </h4>
+
+    <p>
+        A notebookok teljesítményét elsősorban a processzor (CPU) teljesítménye határozza meg. Jelenleg két gyártó, az Intel és az AMD kínálatából választhatunk. Az Intelnél növekvő sorrendben Atom, Celeron, Pentium, Core i3, Core i5 és Core i7 processzorokat találhatunk, az AMD-nél a sorrend így alakul: E1, E2, A4, A6, A10, A12.
+    </p>
+
+    <p>
+        Fontos, hogy felmérjük mire szeretnénk pontosan használni a notebookunkat, mert amennyiben nem használjuk ki az erősebb processzorok teljesítményét, sok pénzt elveszíthetünk a választásával, ráadásul többet is fogyaszt. Gamer laptopok esetén a processzor nagysága nem elhanyagolható és egy gamer laptophűtő néhány esetben elengedhetetlen.
+    </p>
+
+    <p>
+        Ne felejtsük el a memória (RAM) méretét is megvizsgálni a választásnál. A notebookok bekapcsoláskor a RAM-ból töltik be a legfontosabb dolgokat (asztal, futó programok). Egy 4GM alatti memóriájú gép esetében éppen ezért úgy érezhetjük, hogy lassú a használata. 
+    </p>
+</section>
 <?php
 include(APP_ROOT . "/resources/views/layouts/footer.php");
 ?>
